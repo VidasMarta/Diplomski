96b4ef2bcd365103288bb2b0a0a9c13c36388e2d
MartaVidas123 <mv53134@fer.hr>
2025-05-14 12:48:24 +0200

    bug fix

--- hyper_param_tuning.py
+++ hyper_param_tuning.py
@@ -16,8 +16,14 @@ MAX_LEN = 256
 BATCH_SIZE = 32
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-def get_dataset_loaders(text_train, tags_train, text_val, tags_val, word_embeddings_model):    
+def train_model(model_args):    
     # Load datasets for train and test
+    dataset_loader = DatasetLoader(DATASET_NAME, settings.DATA_PATH)
+    tag_to_num, (text_train, tags_train), (text_val, tags_val), (_, _) = dataset_loader.load_data()
+    num_to_tag = dict((v,k) for k,v in tag_to_num.items())
+    word_embeddings_model = Embedding.create("bioBERT", dataset_loader.dataset_name, MAX_LEN) 
+    eval = Evaluation(word_embeddings_model, "IOB1")
+    
     tokens_train_padded, tags_train_padded, attention_masks_train, crf_mask_train = word_embeddings_model.tokenize_and_pad_text(text_train, tags_train)
     train_data = Dataset(tokens_train_padded, tags_train_padded, attention_masks_train, crf_mask_train)
     tokens_val_padded, tags_val_padded, attention_masks_val, crf_mask_val = word_embeddings_model.tokenize_and_pad_text(text_val, tags_val)
@@ -25,7 +31,8 @@ def get_dataset_loaders(text_train, tags_train, text_val, tags_val, word_embeddi
     train_data_loader = torch.utils.data.DataLoader(train_data, batch_size=BATCH_SIZE)
     valid_data_loader = torch.utils.data.DataLoader(val_data, batch_size=BATCH_SIZE)
 
-    return train_data_loader, valid_data_loader
+    return trainer.Finetuning_Trainer(MODEL_NAME, model_args, len(tag_to_num), train_data_loader, valid_data_loader, word_embeddings_model, model_args['char_emb'], 
+                                      text_train, text_val, MAX_LEN, BATCH_SIZE, DEVICE, num_to_tag, eval, None).train(False)
 
 def objective(trial): #TODO otkomentirati ostale za hiperparam za tuning, trenutno samo 2 tunam, ovo je testna verzija
     # Hyperparameters to tune
@@ -40,14 +47,14 @@ def objective(trial): #TODO otkomentirati ostale za hiperparam za tuning, trenut
         model_args['att_num_of_heads'] = trial.suggest_categorical("att_num_of_heads", [4, 8, 16])
     model_args['char_cnn_embedding'] = False #trial.suggest_categorical("char_cnn_embedding", [False, True])
     if model_args['char_cnn_embedding']:
-        model_args['cnn_embedding_dim'] = trial.suggest_categorical("cnn_embedding_dim", [128, 256])
+        model_args['char_embedding_dim'] = trial.suggest_categorical("char_embedding_dim", [128, 256])
         feature_size = trial.suggest_categorical("feature_size", [128, 256])  
         vocab = "abcdefghijklmnopqrstuvwxyz0123456789-,;.!?:'\"/\\|_@#$%^&*~`+-=<>()[]{}"
         max_word_len = 20
-        model_args['char_emb'] = CharEmbeddingCNN(vocab, model_args['cnn_embedding_dim'], feature_size, max_word_len)
+        model_args['char_emb'] = CharEmbeddingCNN(vocab, model_args['char_embedding_dim'], feature_size, max_word_len)
     else:
         model_args['char_emb'] = None
-        model_args['cnn_embedding_dim'] = None
+        model_args['char_embedding_dim'] = None
 
     # Other hyperparameters
     model_args['use_crf'] = True
@@ -55,18 +62,8 @@ def objective(trial): #TODO otkomentirati ostale za hiperparam za tuning, trenut
     model_args['epochs'] = 10 #tako da kraće traje treniranje
     model_args['max_grad_norm'] = 5.0
     model_args['early_stopping'] = 3
-    
-    dataset_loader = DatasetLoader(DATASET_NAME, settings.DATA_PATH)
-    tag_to_num, (text_train, tags_train), (text_val, tags_val), (_, _) = dataset_loader.load_data()
-    num_to_tag = dict((v,k) for k,v in tag_to_num.items())
-    word_embeddings_model = Embedding.create("bioBERT", dataset_loader.dataset_name, MAX_LEN) 
-    eval = Evaluation(word_embeddings_model, "IOB1")
-    train_data_loader, valid_data_loader = get_dataset_loaders(text_train, tags_train, text_val, tags_val, word_embeddings_model)
 
-    f1_valid = trainer.Finetuning_Trainer(MODEL_NAME, model_args, len(tag_to_num), train_data_loader, valid_data_loader, word_embeddings_model, model_args['char_emb'], text_train, text_val, MAX_LEN,
-                                BATCH_SIZE, DEVICE, num_to_tag, eval, None).train(False)
-
-    return f1_valid 
+    return train_model(model_args) 
 
 def main():
     study = optuna.create_study(direction='maximize') #gledat će f1 na val skupu pa treba maksimizirati
