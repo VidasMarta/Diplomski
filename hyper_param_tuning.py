bd7829ced75a454750931285b6819718cdc35a87
MartaVidas123 <mv53134@fer.hr>
2025-05-23 11:47:54 +0200

    changed hyperparams to optimize

--- hyper_param_tuning.py
+++ hyper_param_tuning.py
@@ -12,8 +12,8 @@ from utils import trainer
 from optuna.visualization import plot_optimization_history, plot_param_importances
 import joblib
 
-DATASET_NAME = "bc5cdr_json" # or "ncbi_disease_json"
-MODEL_NAME = "D2_hyper_param_tuning_1" #D1
+DATASET_NAME = "ncbi_disease_json" # or "bc5cdr_json"
+MODEL_NAME = "D1_hyper_param_tuning_3" #D1
 
 def train_model(model_args):    
     # Load datasets for train and test
@@ -31,24 +31,24 @@ def train_model(model_args):
     valid_data_loader = torch.utils.data.DataLoader(val_data, batch_size=model_args['batch_size'])
 
     return trainer.Finetuning_Trainer(MODEL_NAME, model_args, len(tag_to_num), train_data_loader, valid_data_loader, word_embeddings_model, model_args['char_emb'], 
-                                      text_train, text_val,  model_args['max_len'], model_args['batch_size'],  model_args['device'], num_to_tag, eval, None).train(False)
+                                    text_train, text_val,  model_args['max_len'], model_args['batch_size'],  model_args['device'], num_to_tag, eval, None).train(False)
 
 def objective(trial): 
     # Hyperparameters to tune
     model_args = {}
-    model_args['hidden_size'] = trial.suggest_categorical('hidden_size', [256, 512, 768])
-    model_args['lr'] = trial.suggest_loguniform('lr', 5e-4, 1.5e-3) #trenutni lr na 1e-3
-    model_args['ft_lr'] = trial.suggest_loguniform('ft_lr', 1e-5, 3e-5) #trenutni ft_lr na 2e-5
-    model_args['optimizer'] = trial.suggest_categorical("optimizer", ["adam", "adamw"])
-    model_args['dropout'] = trial.suggest_uniform("dropout", 0.15, 0.45)
+    model_args['hidden_size'] = 256 #trial.suggest_categorical('hidden_size', [256, 512, 768])
+    model_args['lr'] = 0.0013367211092689295 #trial.suggest_float('lr', 5e-4, 1.5e-3, log=True) #trenutni lr na 1e-3
+    model_args['ft_lr'] = 2.7170546791615555e-05 #trial.suggest_float('ft_lr', 1e-5, 3e-5, log=True) #trenutni ft_lr na 2e-5
+    model_args['optimizer'] = "adamw" #trial.suggest_categorical("optimizer", ["adam", "adamw"])
+    model_args['dropout'] = 0.31302679090368124 #trial.suggest_uniform("dropout", 0.15, 0.45)
 
-    model_args['attention'] = True #trial.suggest_categorical("attention", [False, True])
+    model_args['attention'] = trial.suggest_categorical("attention", [False, True])
     if model_args['attention']:
-        model_args['att_num_of_heads'] = 16  # trial.suggest_categorical("att_num_of_heads", [4, 8, 16])
-    model_args['char_cnn_embedding'] = True #trial.suggest_categorical("char_cnn_embedding", [False, True])
+        model_args['att_num_of_heads'] = trial.suggest_categorical("att_num_of_heads", [4, 8, 16])
+    model_args['char_cnn_embedding'] = trial.suggest_categorical("char_cnn_embedding", [False, True])
     if model_args['char_cnn_embedding']:
-        model_args['char_embedding_dim'] = 256 #trial.suggest_categorical("char_embedding_dim", [128, 256])
-        feature_size = 256 # trial.suggest_categorical("feature_size", [128, 256])  
+        model_args['char_embedding_dim'] = trial.suggest_categorical("char_embedding_dim", [128, 256])
+        feature_size = trial.suggest_categorical("feature_size", [128, 256])  
         vocab = "abcdefghijklmnopqrstuvwxyz0123456789-,;.!?:'\"/\\|_@#$%^&*~`+-=<>()[]{}"
         max_word_len = 20
         model_args['char_emb'] = CharEmbeddingCNN(vocab, model_args['char_embedding_dim'], feature_size, max_word_len)
@@ -70,13 +70,8 @@ def objective(trial):
 
     return train_model(model_args) 
 
-def main():
-    study = optuna.create_study(direction='maximize') #gledat će f1 na val skupu pa treba maksimizirati
-    study.optimize(objective, n_trials=70) #staviti na 50 ili 100
-    print("Best Hyperparameters:", study.best_params)
-
-    joblib.dump(study, f"/lustre/home/mvidas/hyperparam_tuning/{MODEL_NAME}/study.pkl") 
-
+def plot_graphs():
+    study = joblib.load(f"/lustre/home/mvidas/hyperparam_tuning/{MODEL_NAME}/study.pkl")
     plotly_config = {"staticPlot": True}
 
     fig = plot_optimization_history(study)
@@ -84,6 +79,14 @@ def main():
 
     fig = plot_param_importances(study)
     fig.show(config=plotly_config)
+
+
+def main():
+    study = optuna.create_study(direction='maximize') #gledat će f1 na val skupu pa treba maksimizirati
+    study.optimize(objective, n_trials=70) #staviti na 50 ili 100
+    print("Best Hyperparameters:", study.best_params)
+
+    joblib.dump(study, f"/lustre/home/mvidas/hyperparam_tuning/{MODEL_NAME}/study.pkl") 
 
 def set_seed(seed: int = 42): ##za reproducility, izvor: https://medium.com/we-talk-data/how-to-set-random-seeds-in-pytorch-and-tensorflow-89c5f8e80ce4
     np.random.seed(seed)
@@ -100,3 +103,4 @@ def set_seed(seed: int = 42): ##za reproducility, izvor: https://medium.com/we-t
 if __name__ == "__main__":
     set_seed()
     main()
+    plot_graphs()
