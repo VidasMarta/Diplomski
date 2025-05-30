137ef32745f208eef23e27a3e43cdddca5fb472d
MartaVidas123 <mv53134@fer.hr>
2025-05-30 16:29:54 +0200

    bug fix

--- models.py
+++ models.py
@@ -32,8 +32,8 @@ class BiRNN_CRF(nn.Module):
         if self.attention:
             self.attention_layer = nn.MultiheadAttention(self.hidden_size*2, model_args['att_num_of_heads'], batch_first=True) # *2 because of bidirectional
             self.hidden2tag_tag = nn.Linear(self.hidden_size*4, self.num_tag) # *2 because of bidirectional
-            
-        self.hidden2tag_tag = nn.Linear(self.hidden_size*2, self.num_tag) # *2 because of bidirectional
+        else:           
+            self.hidden2tag_tag = nn.Linear(self.hidden_size*2, self.num_tag) # *2 because of bidirectional
 
         if self.use_crf:
             self.crf_tag = CRF(self.num_tag)
